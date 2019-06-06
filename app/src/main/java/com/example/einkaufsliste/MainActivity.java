c229cc43b9bd621689b3cff902f972c2689304f5
Micha <micha@konzeptplus.ch>
2019-06-06 16:59:49 +0200

    Init

---
+++ app/src/main/java/com/example/einkaufsliste/MainActivity.java
@@ -0,0 +1,66 @@
+package com.example.einkaufsliste;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+	private static final String TAG = MainActivity.class.getSimpleName();
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		BottomNavigationView navView = findViewById(R.id.nav_view);
+		navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+		loadFragment(new ShopListFragment());
+	}
+
+	private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+			= new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+		@Override
+		public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+			Fragment fragment;
+			switch (item.getItemId()) {
+				case R.id.navigation_list:
+					fragment = new ShopListFragment();
+					loadFragment(fragment);
+					return true;
+				case R.id.navigation_places:
+					fragment = new PlacesFragment();
+					loadFragment(fragment);
+					return true;
+				case R.id.navigation_map:
+					fragment = new MapFragment();
+					loadFragment(fragment);
+					return true;
+				case R.id.navigation_settings:
+					fragment = new SettingsFragment();
+					loadFragment(fragment);
+					return true;
+			}
+			return false;
+		}
+	};
+
+	@Override
+	public void onBackPressed() {
+		super.onBackPressed();
+		BottomNavigationView navView = findViewById(R.id.nav_view);
+		MenuItem item = navView.getMenu().getItem(0);
+		navView.setSelectedItemId(item.getItemId());
+	}
+
+	private void loadFragment(Fragment fragment) {
+		// load fragment
+		FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+		transaction.replace(R.id.frame_container, fragment);
+		transaction.addToBackStack(null);
+		transaction.commit();
+	}
+}
---
+++ app/src/main/java/com/example/einkaufsliste/ShopListFragment.java
@@ -0,0 +1,29 @@
+package com.example.einkaufsliste;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ShopListFragment extends Fragment {
+
+
+	public ShopListFragment() {
+		// Required empty public constructor
+	}
+
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+							 Bundle savedInstanceState) {
+		// Inflate the layout for this fragment
+		return inflater.inflate(R.layout.fragment_shop_list, container, false);
+	}
+
+}
